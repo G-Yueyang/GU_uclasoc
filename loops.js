e09b10240cea9d2e2c75c377eeb5bf92de4f37f7
Guilty <Guilty@GuiltydeMacBook-Pro.local>
2018-07-27 13:17:05 -0700

    initial commit

---
+++ loops.js
@@ -0,0 +1,19 @@
+var animals = ['zebra', 'line', 'tiger', 'elephant', 'monkey'];
+
+var professions = ['doctor', 'lawyer', 'programmer', 'ballerina'];
+
+var students = ['Emma', 'Yang', 'PepeJ', 'PepeV', 'Ethan', 'Alison', 'Nico', 'Brian', 'Uli', 'Caleb', 'Rawad', 'Hassan', 'Yueyang', 'Lyla'];
+
+var numbers = [12, 457, 52, 16];
+
+var largestAnimalName = animals.filter(animal => animal.length > 4);
+
+var capitalize = professions.map(profession => profession.toUpperCase());
+
+var awesome = students.map(student => student+" is awesome");
+
+var total = numbers.reduce((memo,currentValue) => memo+currentValue,0);
+
+var total = numbers.reduce((memo,currentValue) => memo*currentValue,1);
+
+var app = student.foreach(function(student){${studnet}ismakinganAPP}）；
